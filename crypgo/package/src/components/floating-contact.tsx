0db639cf91f0a4e0a259553a33a8d5b7953a2098
Gonzalo Rojas <77162084+grojasc@users.noreply.github.com>
2025-09-05 21:38:52 -0400

    fix wsp

--- crypgo/package/src/components/floating-contact.tsx
+++ crypgo/package/src/components/floating-contact.tsx
@@ -20,7 +20,7 @@ const FloatingContact = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       className="
-        fixed bottom-6 right-6 w-14 h-14
+        fixed bottom-6 right-20 w-14 h-14
         bg-green-500 hover:bg-green-600 text-white rounded-full
         flex items-center justify-center shadow-lg hover:shadow-xl
         transition-all duration-300 z-50 group
